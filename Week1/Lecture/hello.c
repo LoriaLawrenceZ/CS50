bd554a70427e2507d2f4c9c1c2f18c061624bce6
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-10-02 14:15:37 -0300

    Adding comments

--- Week1/Lecture/hello.c
+++ Week1/Lecture/hello.c
@@ -2,11 +2,11 @@
 #include <stdio.h> //Standart IO librarie
 
 int main(void){
-    char answer[50];
+    char name[50]; //"String" to store the user's name
 
-    printf("Hello, world!\nWhat's yout name? ");
+    printf("Hello, world!\nWhat's yout name? "); //Asking the user's name
     //Getting and saving the text
-    fgets(answer, sizeof(answer), stdin); //Function "fgets()" need these 3 parameters to work
+    fgets(name, sizeof(name), stdin); //Function "fgets()" need these 3 parameters to work
 
-    printf("Hello, %s", answer);
+    printf("Hello, %s", name); //Printing user's name
 }
