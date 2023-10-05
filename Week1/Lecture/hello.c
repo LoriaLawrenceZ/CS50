6ee253dd2dba60baf92f2ea118356076c62966f0
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-10-05 02:14:16 +0000

    Learning conditionals in C language

--- Week1/Lecture/hello.c
+++ Week1/Lecture/hello.c
@@ -2,6 +2,7 @@
 #include <stdio.h> //Standart IO librarie
 
 int main(void){
+    //-----===Asking User's Name===-----//
     char name[50]; //"String" to store the user's name
 
     printf("Hello, world!\nWhat's yout name? "); //Asking the user's name
@@ -9,4 +10,12 @@ int main(void){
     fgets(name, sizeof(name), stdin); //Function "fgets()" need these 3 parameters to work
 
     printf("Hello, %s", name); //Printing user's name
+
+    //-----===Conditionals===-----//
+    if (x < y) {
+        printf("X is less than Y\n");
+    }
+    else {
+        printf("X not less than Y");
+    }
 }
